f719d7ffdb96433a0659544c70f848c8c2d5b87c
Mariano Flores <mariano.flores@xero.com>
2015-04-14 16:49:43 +1200

    adding a powershell file to restart the services

---
+++ README.md
@@ -0,0 +1,5 @@
+#wacom
+
+Workaround to the memory leak listed here:
+
+http://forum.wacom.eu/viewtopic.php?f=4&t=20807
---
+++ services_restart.ps1
@@ -0,0 +1,7 @@
+Stop-Service TabletInputService
+Start-Sleep -s 10
+Start-Service TabletInputService
+Start-Sleep -s 10
+Stop-Service WTabletServiceCon
+Start-Service WTabletServiceCon
+
